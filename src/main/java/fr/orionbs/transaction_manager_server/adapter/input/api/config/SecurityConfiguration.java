c6277190194bcb3c661ffea47a3738ab453b8eeb
OrionBS <obeaunysugot@gmail.com>
2023-07-19 19:45:02 +0200

    change actuator access.

--- src/main/java/fr/orionbs/transaction_manager_server/adapter/input/api/config/SecurityConfiguration.java
+++ src/main/java/fr/orionbs/transaction_manager_server/adapter/input/api/config/SecurityConfiguration.java
@@ -34,6 +34,7 @@ public class SecurityConfiguration {
 
         httpSecurity.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
             authorizationManagerRequestMatcherRegistry.requestMatchers(HttpMethod.GET, "/v3/api-docs/**", "/swagger-ui/**").permitAll();
+            authorizationManagerRequestMatcherRegistry.requestMatchers("/actuator/**");
         });
 
 
